4eca6dd81f87c5e3f70ded8f4d87544e602ce02f
Amarnath Sah <sahamarnath79@gmail.com>
2024-02-14 20:16:03 +0545

    FIX: fixing error in console

--- src/main/java/com/esper/cepengine/interfaces/EsperConfigurationCustomizer.java
+++
@@ -1,8 +0,0 @@
-package com.esper.cepengine.interfaces;
-
-public interface EsperConfigurationCustomizer {
-
-    void customize(com.espertech.esper.common.client.configuration.Configuration configuration) throws Exception;
-
-
-}
