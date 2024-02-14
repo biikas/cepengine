ddaf3a3a1302283fae70221d54374a675827a38d
Amarnath Sah <sahamarnath79@gmail.com>
2024-02-14 19:35:37 +0545

    FIX: code refactor and added some logic

---
+++ src/main/java/com/esper/cepengine/listener/EsperListner.java
@@ -0,0 +1,33 @@
+package com.esper.cepengine.listener;
+
+import com.espertech.esper.client.EventBean;
+import com.espertech.esper.client.UpdateListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EsperListner implements UpdateListener {
+
+    @Override
+    public void update(EventBean[] newData, EventBean[] oldData) {
+        for (EventBean event : newData) {
+            String magnitude = (String) event.get("magnitude");
+            String location = (String) event.get("location");
+            String topic = (String) event.get("topic");
+
+            handleCriticalEvent(magnitude, location,topic);
+        }
+    }
+
+
+    private void handleCriticalEvent(String magnitude, String location, String topic) {
+        if (magnitude != null && location != null) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("***************************************\n");
+            sb.append("* [ALERT] : CRITICAL EVENT DETECTED from topic: ").append(topic).append("\n");
+            sb.append("* Location: ").append(location).append(", Magnitude: ").append(magnitude).append("\n");
+            sb.append("***************************************\n");
+            System.out.println(sb);
+        }
+    }
+
+}
--- src/main/java/com/esper/cepengine/manager/CEPInstanceManager.java
+++ src/main/java/com/esper/cepengine/manager/CEPInstanceManager.java
@@ -2,6 +2,8 @@ package com.esper.cepengine.manager;
 
 import com.esper.cepengine.dto.Earthquake;
 import com.esper.cepengine.espersclass.EsperInstance;
+import com.espertech.esper.client.EPServiceProvider;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +15,10 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class CEPInstanceManager {
+
+    private EPServiceProvider epService;
 
     private final Map<String, EsperInstance> esperInstances = new ConcurrentHashMap<>();
 
